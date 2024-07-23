6dea72814ef0839e5bcefc2395692185e1085167
PaulKorDev <pauldeveloper@outlook.com>
2024-07-23 06:00:13 -0700

    Squashed commit of the following:
    commit ed663dd241e0a2f165c5b5f004bc3bd922837f41
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 23 05:58:33 2024 -0700
    
        Import DOTween
    
    commit baa2f23d427c3be4cc9bb0475a059581b7f768a4
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 23 05:16:55 2024 -0700
    
        Clamp x movement position
    
    commit e6a77f2c42f6fce645a3d1e1293e8c40fb94aa93
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 23 05:03:46 2024 -0700
    
        Add chunks for building level
    
    commit f4d3fd36908113deef364d6f91486dbc2c2f5d3f
    Merge: 1fe7ba0 cfd2808
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 23 04:33:32 2024 -0700
    
        Merge branch 'main' into Movement
    
    commit 1fe7ba02aad7d18e55c10b839384b8fa847f53eb
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 23 03:32:02 2024 -0700
    
        Some Refactor Move Script
    
    commit 74d50ffc4843afa51cf17d4da42bde2829233a13
    Author: PaulKorDev <pauldeveloper@outlook.com>
    Date:   Tue Jul 23 03:27:34 2024 -0700
    
        Add slide player movement

---
+++ Assets/_development/Scripts/Mechanics/Movement/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+namespace Assets._development.Scripts.Movement
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("Speed movement")]
+        [SerializeField] private float _playerForwardSpeed;
+        [SerializeField] private float _playerSlideSpeed;
+
+        [Header("LimitMovement")]
+        [SerializeField] private float _limitWidth; //This value can be setted from chunk width or you can create movement area, so I decide set value manually because it simpler and faster)
+
+        private Vector3 _clickScreenPosition;
+        private Vector3 _clickPlayerPosition;
+
+        private float _invertedScreenWidth = 1f / Screen.width;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            MoveForward();
+            MoveSlide();
+        }
+
+        private void MoveForward()
+        {
+            transform.position += Vector3.forward * Time.deltaTime * _playerForwardSpeed;
+        }
+        private void MoveSlide()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _clickScreenPosition = Input.mousePosition;
+                _clickPlayerPosition = transform.position;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                GetOffsetX(out float xOffset);
+                MovePlayerToNewSidePosition(xOffset);
+            }
+        }
+        private void GetOffsetX(out float xOffset)
+        {
+            xOffset = Input.mousePosition.x - _clickScreenPosition.x;
+            xOffset *= _playerSlideSpeed * _invertedScreenWidth;
+        }
+        private void MovePlayerToNewSidePosition(float xOffset)
+        {
+            var halfOfLimitMovement = _limitWidth * 0.5f;
+
+            Vector3 newPlayerPosition = transform.position;
+            var clampedPositionX = Mathf.Clamp(_clickPlayerPosition.x + xOffset, -halfOfLimitMovement, halfOfLimitMovement);
+            newPlayerPosition.x = clampedPositionX;
+
+            transform.position = newPlayerPosition;
+        }
+    }
+}
