ed663dd241e0a2f165c5b5f004bc3bd922837f41
PaulKorDev <pauldeveloper@outlook.com>
2024-07-23 05:58:33 -0700

    Import DOTween

---
+++ Assets/_development/Scripts/Player/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int _earnedMoney;
+    private int _moneyToProsperous;
+    private int _moneyToRich;
+
+
+    public void Init()
+    {
+        //Init called when level is loaded/restarted
+        //Set _earnedMoney to 0
+        //
+    }
+}
