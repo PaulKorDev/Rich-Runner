882beee742760607a236441429301165d644b2d5
PaulKorDev <pauldeveloper@outlook.com>
2024-07-24 00:13:52 -0700

    Hide MainUI when started

--- Assets/ToDoList.cs
+++ Assets/ToDoList.cs
@@ -2,10 +2,10 @@
  * 
 LEFT:
 v 0. Change Idle animation
-1. Changing of model
+v 1. Changing of model
 2. Clamp health and die
 2. Progress bar
-3. Game UI (baalnce and level)
+3. Game UI (balance and level)
 4. Switching UI
 
 
---
+++ Assets/_development/Scripts/View/UIRootView.cs
@@ -0,0 +1,23 @@
+﻿using Assets._development.Scripts.Architecture.EventBus;
+using Assets.Scripts.Architecture.ServiceLocator;
+using UnityEngine;
+
+namespace Assets._development.Scripts.View
+{
+    public class UIRootView : MonoBehaviour
+    {
+        private void Start()
+        {
+            ServiceLocator.Get<GameplayEventBus>().OnLevelStarted.Subscribe(HideMainUI);
+        }
+
+        private void ShowMainUI()
+        {
+            gameObject.SetActive(true);
+        }
+        private void HideMainUI()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
